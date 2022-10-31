20e1a572d7c0bf5abb133645ce63d5e8728991c1
Damien Garnier <94621541+damigarn@users.noreply.github.com>
2022-10-31 15:28:09 +0100

    Commit 4: Loop on categories dictionary creating a list of all the books of the category

--- main.py
+++ main.py
@@ -35,3 +35,37 @@ cat_data = Path.cwd() / "Data"
 if cat_data.exists():
     shutil.rmtree(cat_data)
 cat_data.mkdir()
+
+# Categories pages exploration
+# Dictionary needed for rating formatting. Here to avoid to be in the loop and to create an instance each time
+dict_rating = {'One': '1/5', 'Two': '2/5', 'Three': '3/5', 'Four': '4/5', 'Five': '5/5'}
+for cat, cat_url in dict_categories.items():
+
+    # Progression category indicator
+    indice_category = list(dict_categories.keys()).index(cat) + 1
+    print(f"\n{indice_category}/{nbCategories} - Category '{cat}'")
+
+    # Connexion and souping cat_url
+    r_cat_url_one = requests.get(cat_url)
+    soup_cat_url_one = BeautifulSoup(r_cat_url_one.content, 'html.parser')
+
+    # Books number indicator
+    nb_livres = int(soup_cat_url_one.find(class_="form-horizontal").find("strong").get_text(strip=True))
+    print(f"Book(s) found : {nb_livres}")
+
+    # Category's list of books urls from category's index page
+    books_links = [link.get('href') for link in soup_cat_url_one.select(".product_pod h3 a")]
+
+    # Category's list of books urls from category's next pages if existed
+    i = 2
+    while True:
+        r_cat_url_next = requests.get(f"{cat_url}/page-{i}.html")
+        if r_cat_url_next.status_code not in [200, 201]:
+            break
+        soup_cat_url_next = BeautifulSoup(r_cat_url_next.content, 'html.parser')
+        books_links_next = [link.get('href') for link in soup_cat_url_next.select(".product_pod h3 a")]
+        books_links.extend(iter(books_links_next))
+        i += 1
+
+    # Relative to absolute links
+    books_links = [link.replace('../../..', f"{HOME_URL}/catalogue") for link in books_links]
