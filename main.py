5401575dbe2f391bcb327d3cda3337e9fb1be814
Damien Garnier <94621541+damigarn@users.noreply.github.com>
2022-10-31 15:23:31 +0100

    Commit 2: Dictionary of Categories and their links form homepage with BeautifulSoup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 HOME_URL = "https://books.toscrape.com"
 
@@ -12,3 +13,15 @@ if r_home.status_code in [200, 201]:
 else:
     print(f"\nConnection to {HOME_URL} is not impossible\n"
           "Exiting script\n")
+
+# Home page soup
+soup_home = BeautifulSoup(r_home.content, 'html.parser')
+
+# Dictionnaire → Clé : Nom de la catégorie ; Valeur : lien index.html de la catégorie
+dict_categories = {}
+for a in soup_home.select(".side_categories ul li ul li a"):
+    dict_categories[a.get_text(strip=True)] = f"{HOME_URL}/{a['href'].replace('/index.html', '')}"
+
+# Print the number of categories
+nbCategories = len(dict_categories)
+print(f"{nbCategories} catégories trouvées.")
