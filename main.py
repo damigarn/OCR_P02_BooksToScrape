8fbd9f0f9f37f22431f3e36ef6e8701a61ed0fb0
Damien Garnier <94621541+damigarn@users.noreply.github.com>
2022-11-08 19:46:31 +0100

    Commit 13: all refactored

--- main.py
+++ main.py
@@ -1,142 +1,35 @@
-from pathlib import Path
-import shutil
-import csv
-
-import requests
-from bs4 import BeautifulSoup
-from tqdm import tqdm
-
-HOME_URL = "https://books.toscrape.com"
+import scraper
 
 
-# Home connection
-r_home = requests.get(HOME_URL)
+# Website to scrape (Script only works with this url HOME is a constant)
+HOME = "https://books.toscrape.com"
 
-# Welcome message
-if r_home.status_code in [200, 201]:
-    print(f"\nConnection to {HOME_URL} is OK\n")
-    print()
-else:
-    print(f"\nConnection to {HOME_URL} is not impossible\n"
-          "Exiting script\n")
 
-# Home page soup
-soup_home = BeautifulSoup(r_home.content, 'html.parser')
+def main():
 
-# Dictionnaire → Clé : Nom de la catégorie ; Valeur : lien index.html de la catégorie
-dict_categories = {}
-for a in soup_home.select(".side_categories ul li ul li a"):
-    dict_categories[a.get_text(strip=True)] = f"{HOME_URL}/{a['href'].replace('/index.html', '')}"
+    # Liste de toutes les catégories et leurs liens associés
+    cat_list = scraper.all_categories(HOME)
 
-# Print the number of categories
-nbCategories = len(dict_categories)
-print(f"{nbCategories} catégories trouvées.")
+    # Category selection
+    cat_select = scraper.cat_selection(cat_list)
 
-# Target directory for database
-cat_data = Path.cwd() / "Data"
-if cat_data.exists():
-    shutil.rmtree(cat_data)
-cat_data.mkdir()
+    # Creation of directories and files to store data
+    data_files = scraper.dir_manager(cat_select)
 
-# Categories pages exploration
-# Dictionary needed for rating formatting. Here to avoid to be in the loop and to create an instance each time
-dict_rating = {'One': '1/5', 'Two': '2/5', 'Three': '3/5', 'Four': '4/5', 'Five': '5/5'}
-for cat, cat_url in dict_categories.items():
+    # Extracting data
+    data = scraper.scraper(cat_select)
 
-    # Progression category indicator
-    indice_category = list(dict_categories.keys()).index(cat) + 1
-    print(f"\n{indice_category}/{nbCategories} - Category '{cat}'")
+    # Formatting data
+    f_data = scraper.format_data(data)
 
-    # Connexion and souping cat_url
-    r_cat_url_one = requests.get(cat_url)
-    soup_cat_url_one = BeautifulSoup(r_cat_url_one.content, 'html.parser')
+    # Loading data
+    scraper.file_writer(f_data, data_files)
 
-    # Books number indicator
-    nb_livres = int(soup_cat_url_one.find(class_="form-horizontal").find("strong").get_text(strip=True))
-    print(f"Book(s) found : {nb_livres}")
+    # Another extraction ?
+    if scraper.one_more():
+        main()
 
-    # Category's list of books urls from category's index page
-    books_links = [link.get('href') for link in soup_cat_url_one.select(".product_pod h3 a")]
 
-    # Category's list of books urls from category's next pages if existed
-    i = 2
-    while True:
-        r_cat_url_next = requests.get(f"{cat_url}/page-{i}.html")
-        if r_cat_url_next.status_code not in [200, 201]:
-            break
-        soup_cat_url_next = BeautifulSoup(r_cat_url_next.content, 'html.parser')
-        books_links_next = [link.get('href') for link in soup_cat_url_next.select(".product_pod h3 a")]
-        books_links.extend(iter(books_links_next))
-        i += 1
-
-    # Relative to absolute links
-    books_links = [link.replace('../../..', f"{HOME_URL}/catalogue") for link in books_links]
-
-    # Category directories for downloading data
-    cat_path = Path.cwd() / "Data" / cat
-    cat_path_images = cat_path / "Images"
-    cat_path.mkdir()
-    cat_path_images.mkdir()
-
-    # Category CSV data file
-    data_file = cat_path / f"{cat}.csv"
-    data_file.touch()
-    fields = ["link",
-              "universal_product_code",
-              "title",
-              "price_including_tax",
-              "price_excluding_tax",
-              "number_available",
-              "product_description",
-              "category",
-              "review_rating",
-              "image_url"]
-    with open(data_file, mode='w') as f:
-        csvwriter = csv.writer(f)
-        csvwriter.writerow(fields)
-
-    # Downloading data
-    for link in tqdm(books_links, ncols=100, desc="Téléchargement"):
-        # Scanning book infos
-        r_book_page = requests.get(link)
-        book_infos = BeautifulSoup(r_book_page.content, 'html.parser').find(class_="product_page")
-
-        # Book infos
-        universal_product_code = book_infos.find("table").select("td")[0].get_text(strip=True)
-        title = book_infos.find(class_="product_main").find("h1").get_text(strip=True)
-        price_including_tax = book_infos.find("table").select("td")[2].get_text(strip=True)
-        price_excluding_tax = book_infos.find("table").select("td")[3].get_text(strip=True)
-        number_available = book_infos.find("table").select("td")[5].get_text(strip=True)
-        review_rating = book_infos.find(class_="star-rating").attrs.get('class')[1]
-        image_url = book_infos.find('img')['src'].replace("../..", HOME_URL)
-        image_name = book_infos.find('img')['alt'].replace(" ", "_").replace("/", "-") + ".jpg"
-        # Description not all the time
-        try:
-            try_descr = book_infos.select("#product_description + p")[0].get_text(strip=True)
-        except IndexError:
-            product_description = ""
-        else:
-            product_description = try_descr
-
-        # Image Download
-        r_image = requests.get(image_url, stream=True)
-        img_path = cat_path_images / image_name
-        with open(img_path, mode='wb') as f:
-            shutil.copyfileobj(r_image.raw, f)
-
-        # Formatting books infos
-        book_format = [link,
-                       universal_product_code,
-                       title.title(),
-                       float(price_including_tax.lstrip("£")),
-                       float(price_excluding_tax.lstrip("£")),
-                       int(number_available.strip(" Instock(available)")),
-                       product_description,
-                       cat.capitalize(),
-                       dict_rating[review_rating],
-                       image_url.replace("../..", HOME_URL)]
-
-        # Saving book infos to data file
-        with open(data_file, mode='a') as f:
-            csvwriter = csv.writer(f)
-            csvwriter.writerow(book_format)
+if __name__ == "__main__" and scraper.connect_status(HOME):
+    print(f"\nConnection to {HOME} is OK")
+    main()
---
+++ scraper.py
@@ -0,0 +1,272 @@
+from pathlib import Path
+import shutil
+import re
+import csv
+
+import requests
+from bs4 import BeautifulSoup
+from terminaltables import AsciiTable
+from tqdm import tqdm
+
+import main
+
+CURRENT_DIR = Path.cwd()
+DATA_DIR = CURRENT_DIR / "data"
+IMAGES_DIR_NAME = "images"
+
+
+def connect_status(link):
+
+    print("\nChecking connection...")
+
+    r = requests.get(link)
+
+    if r.status_code in [200, 201]:
+        return True
+
+    print(f"\nConnection to {link} is not impossible"
+          "Exiting script\n")
+    exit()
+
+
+def all_categories(link):
+
+    # Home connection
+    r_home = requests.get(link)
+
+    # Home page soup
+    soup_home = BeautifulSoup(r_home.content, 'html.parser')
+
+    # Categories list
+    cat_list = []
+    souped_links = soup_home.select(".side_categories ul li ul li a")
+    for a in souped_links:
+        cat_name = a.get_text(strip=True)
+        cat_link = f"{main.HOME}/{a['href'].replace('/index.html', '')}"
+        cat_list.append({'name': cat_name, 'link': cat_link})
+
+    # Alphabetical order of list's categories
+    cat_list.sort(key=lambda x: x['name'])
+
+    return cat_list
+
+
+def cat_selection(cat_list: list):
+
+    # Header
+    print(f"\nListe des catégories trouvées")
+
+    # Categories table
+    cat_table = [['ID', 'Category', 'link']]
+    cat_table.extend([f"[{cat_list.index(i) + 1}]", i['name'], i['link']] for i in cat_list)
+    print(AsciiTable(cat_table).table)
+
+    # User choice
+    while True:
+        user = input("Enter the category [index] to download it or [All] for all categories at once: ").lower()
+        if user == 'all':
+            return cat_list
+        elif int(user) in range(1, len(cat_list) + 1):
+            return [cat_list[int(user) - 1]]
+        else:
+            print("Invalid input.")
+
+
+def dir_manager(cat_list: list):
+    """
+    Target directories for files and downloads
+    :param cat_list: Categories list which will be extracted form the website
+    :return:
+    """
+
+    if not DATA_DIR.exists():
+        DATA_DIR.mkdir()
+
+    file_paths = {}
+
+    for cat in cat_list:
+
+        # Categories directories
+        cat_path = DATA_DIR / cat['name']
+        if cat_path.exists():
+            shutil.rmtree(cat_path)
+        cat_path.mkdir()
+        cat_path_images = cat_path / IMAGES_DIR_NAME
+        cat_path_images.mkdir()
+
+        # Output data file
+        data_file = cat_path / f"{cat['name']}.csv"
+        data_file.touch()
+        file_paths[cat['name']] = data_file
+
+    return file_paths
+
+
+def scraper(cat_list: list):
+
+    scraped_data = {}
+
+    for cat in cat_list:
+
+        cat_books = []
+
+        # Category scraping
+        books = category_scraper(cat['link'])
+
+        # Header of each downloading bar
+        print(f"\n{cat_list.index(cat) + 1 }/{len(cat_list)} - Category '{cat['name']}': {books[0]} book(s) found.")
+
+        # Books scraping
+        dir_cat = DATA_DIR / cat['name']
+        for book_link in tqdm(books[1], ncols=100, desc="Téléchargement"):
+            book_infos = book_scraper(cat['name'], book_link, dir_cat)
+            cat_books.append(book_infos)
+
+        scraped_data[cat['name']] = cat_books
+
+    return scraped_data
+
+
+def category_scraper(cat_home):
+
+    # Connexion and souping cat_url
+    r_cat_home = requests.get(cat_home)
+    soup_cat_home = BeautifulSoup(r_cat_home.content, 'html.parser')
+
+    # Books number
+    nb_books = int(soup_cat_home.find(class_="form-horizontal").find("strong").get_text(strip=True))
+
+    # Category's list of books urls from category's index page
+    books_links = [link.get('href') for link in soup_cat_home.select(".product_pod h3 a")]
+
+    # Category's list of books urls from category's next pages if existed
+    i = 2
+    while True:
+        r_cat_next = requests.get(f"{cat_home}/page-{i}.html")
+        if r_cat_next.status_code not in [200, 201]:
+            break
+        soup_cat_url_next = BeautifulSoup(r_cat_next.content, 'html.parser')
+        books_links_next = [link.get('href') for link in soup_cat_url_next.select(".product_pod h3 a")]
+        books_links.extend(iter(books_links_next))
+        i += 1
+
+    # Relative to absolute links
+    books_links = [link.replace('../../..', f"{main.HOME}/catalogue") for link in books_links]
+
+    return nb_books, books_links
+
+
+def book_scraper(cat, link, cat_dir):
+
+    # Connexion and souping book page
+    r_book = requests.get(link)
+    soup_book = BeautifulSoup(r_book.content, 'html.parser').find(class_="product_page")
+
+    # Book infos
+    title = soup_book.find(class_="product_main").find("h1").get_text(strip=True)
+    soup_table = soup_book.find("table")
+    upc = soup_table.select("td")[0].get_text(strip=True)
+    price_it = soup_table.select("td")[2].get_text(strip=True)
+    price_et = soup_table.select("td")[3].get_text(strip=True)
+    stock = soup_table.select("td")[5].get_text(strip=True)
+    stars = soup_book.find(class_="star-rating").attrs.get('class')[1]
+    # Description not all the time
+    try:
+        try_descr = soup_book.select("#product_description + p")[0].get_text(strip=True)
+    except IndexError:
+        p_descr = ""
+    else:
+        p_descr = try_descr
+
+    # Image Download
+    image_url = soup_book.find('img')['src'].replace("../..", main.HOME)
+    r_image = requests.get(image_url, stream=True)
+    image_name = soup_book.find('img')['alt'].replace(" ", "_").replace("/", "-") + ".jpg"
+    img_path = cat_dir / IMAGES_DIR_NAME / image_name
+    with open(img_path, mode='wb') as f:
+        shutil.copyfileobj(r_image.raw, f)
+
+    return {"link": link,
+            "upc": upc,
+            "title": title,
+            "price_it": price_it,
+            "price_et": price_et,
+            "stock": stock,
+            "p_descr": p_descr,
+            "cat": cat,
+            "stars": stars,
+            "img_url": image_url}
+
+
+def format_data(data: dict):
+
+    # Dictionary used to transform rating from letters to numbers
+    rating_dict = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+
+    # Formatting data
+    for books_list in data.values():
+        for book in books_list:
+            setup_format(book, rating_dict)
+    return data
+
+
+def setup_format(book, rating_dict):
+    book["title"] = book["title"].title()
+    book["price_it"] = float(book["price_it"].lstrip("£"))
+    book["price_et"] = float(book["price_et"].lstrip("£"))
+    book["stock"] = re.sub(r"\D", "", book["stock"])
+    book["cat"] = book["cat"].capitalize()
+    book["stars"] = rating_dict[book["stars"]]
+    book["img_url"] = book["img_url"].replace("../..", main.HOME)
+
+
+def file_writer(data: dict, file_paths):
+
+    fields_name = ["product_page_url",
+                   "universal_product_code",
+                   "title",
+                   "price_including_tax",
+                   "price_excluding_tax",
+                   "number_available",
+                   "product_description",
+                   "category",
+                   "review_rating",
+                   "image_url"]
+
+    for cat, value in data.items():
+
+        file_path = file_paths[cat]
+
+        with open(file_path, mode='w') as f:
+            csvwriter = csv.writer(f)
+            csvwriter.writerow(fields_name)
+
+        for book in value:
+            data_to_csv = [book["link"],
+                           book["upc"],
+                           book["title"],
+                           book["price_it"],
+                           book["price_et"],
+                           book["stock"],
+                           book["p_descr"],
+                           book["cat"],
+                           book["stars"],
+                           book["img_url"]]
+
+            # Saving book infos to data file
+            with open(file_path, mode='a') as f:
+                csvwriter = csv.writer(f)
+                csvwriter.writerow(data_to_csv)
+
+        print("Data successfully saved.\n")
+
+
+def one_more():
+    while True:
+        another = input("Do you want to download another category? [y] or [N]: ").lower()
+        if another == "y":
+            return True
+        elif another == "n":
+            exit()
+        else:
+            print("Invalid input.")
